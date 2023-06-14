4e02912298c9bf3d45fccb1f89cfe49c44cc3523
RAJAT VERMA <rv5393982@gmail.com>
2023-06-14 15:41:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <button className="buttonstyle">
+    Semester 1
+  </button>
+  <button className="buttonstyle">
+    Semester 2
+  </button>
+  <br/>
+  <button className="buttonstyle">
+    Semester 3
+  </button>
+  <button className="buttonstyle">
+    Semester 4
+  </button>
+  <br/>
+
+  
+  </>
+  
   );
 }
 
