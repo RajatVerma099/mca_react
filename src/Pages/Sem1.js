734e046f145f45a39c48c51c113f4a45348cbfca
RAJAT VERMA <rv5393982@gmail.com>
2023-06-21 00:36:26 +0530

    accordian 4 try

--- src/Pages/Sem1.js
+++ src/Pages/Sem1.js
@@ -8,10 +8,10 @@ const AccordionItem = ({ title, content }) => {
     const currentTime = new Date().getTime();
     if (currentTime - lastClickTime < 300) {
       // Double tap occurred
-      setIsOpen(false);
+      setIsOpen(!isOpen);
     } else {
       // Single tap occurred
-      setIsOpen(!isOpen);
+      setIsOpen(true);
     }
     setLastClickTime(currentTime);
   };
@@ -52,51 +52,3 @@ const NestedAccordion = () => {
 };
 
 export default NestedAccordion;
-
-
-
-// import React, { useState } from 'react';
-
-// const AccordionItem = ({ title, content }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleAccordion = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <div className="accordion-item">
-//       <div className="accordion-header" onClick={toggleAccordion}>
-//         {title}
-//       </div>
-//       {isOpen && <div className="accordion-content">{content}</div>}
-//     </div>
-//   );
-// };
-
-// const NestedAccordion = () => {
-//   return (
-//     <div className="nested-accordion">
-//       <AccordionItem
-//         title="Section 1"
-//         content={
-//           <div>
-//             <p>Content of section 1</p>
-//             <NestedAccordion />
-//           </div>
-//         }
-//       />
-//       <AccordionItem
-//         title="Section 2"
-//         content={
-//           <div>
-//             <p>Content of section 2</p>
-//             <NestedAccordion />
-//           </div>
-//         }
-//       />
-//     </div>
-//   );
-// };
-
-// export default NestedAccordion;
