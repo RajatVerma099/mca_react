d410318d35f311217557bf5d2f27704e413d461f
RAJAT VERMA <rv5393982@gmail.com>
2023-06-20 16:27:51 +0530

    build

--- src/Pages/PdfPage.js
+++ src/Pages/PdfPage.js
@@ -6,7 +6,6 @@ export const PdfPage=(props)=>
     console.log(props)
 return (
     <div className='container' style={{height:"100vh"}}>
-        {/* <h1>{props.title}</h1> */}
         <iframe src={props.link} width={"100%"} height={"100%"} ></iframe>
     </div>
     )
