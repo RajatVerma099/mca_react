662c79f14c745fbcce9e417e78f138e7fb294155
RAJAT VERMA <rv5393982@gmail.com>
2023-06-23 15:25:52 +0530

    aaccordian fixed and maths gdrive link

--- src/Pages/Sem1.js
+++ src/Pages/Sem1.js
@@ -30,11 +30,17 @@ const NestedAccordion = () => {
   return (
     <div className="nested-accordion">
       <AccordionItem
-        title="Section 1"
+        title="Linear Algebra, Probability & Statistics"
         content={
           <div>
-            <p>Content of section 1</p>
-            <NestedAccordion />
+            <a href='https://drive.google.com/uc?export=view&id=1aIQF6gxiDZO_RbfHxySo4oKZ8fpZSP6U'><p> Linear Algebra Book</p></a>
+            <p>Unit 1.) Vector Space</p>
+            <p>Unit 2.) Linear Transformation</p>
+            <p>Unit 3.) Probability and Random Variables</p>
+            <p>Unit 4.) Testing of Hypothesis</p>
+            <p>Unit 5.) Design Of Hypothesis</p>
+
+            {/* <NestedAccordion /> */}
           </div>
         }
       />
@@ -43,7 +49,7 @@ const NestedAccordion = () => {
         content={
           <div>
             <p>Content of section 2</p>
-            <NestedAccordion />
+            {/* <NestedAccordion /> */}
           </div>
         }
       />
