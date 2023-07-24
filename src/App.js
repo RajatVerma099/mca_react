4737a553d342c76a16ab3b575d05fe5c3b34c22f
Rajat <rv5393982@gmail.com>
2023-07-24 16:19:31 +0530

    change3

--- src/App.js
+++ src/App.js
@@ -38,10 +38,10 @@ function App() {
       <div>  </div>
       </center>
     </Router>
-{/* <img 
+<img 
 src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
 alt="new"
-/> */}
+/>
     </>
   );
 }
